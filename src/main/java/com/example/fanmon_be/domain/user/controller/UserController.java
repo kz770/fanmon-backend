099bc18338104d6c3035010a9ef54892c31f1f81
qordpsem <qordpsem@gmail.com>
2024-09-30 11:40:53 +0900

    [Feat] spring security setting
    - build.gradle 에 swagger 추가
    - security, swagger config 추가
    - UserDAO 추가

--- src/main/java/com/example/fanmon_be/domain/user/controller/UserController.java
+++ src/main/java/com/example/fanmon_be/domain/user/controller/UserController.java
@@ -1,8 +1,17 @@
 package com.example.fanmon_be.domain.user.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.fanmon_be.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
 
+    @GetMapping("/test2")
+    public String test(){
+        return "test";
+    }
 }
