7c973ade54617633fbc4bc6f146e45c9d2a7d694
uhyunjin <uhyeonjin0619@gmail.com>
2024-09-30 21:33:30 +0900

    [Feat] 구독 확인 기능 작성

--- src/main/java/com/example/fanmon_be/domain/chat/controller/ChatController.java
+++ src/main/java/com/example/fanmon_be/domain/chat/controller/ChatController.java
@@ -4,8 +4,6 @@ import com.example.fanmon_be.domain.chat.entity.Subscribe;
 import com.example.fanmon_be.domain.chat.service.ChatService;
 import com.example.fanmon_be.domain.chat.service.SubscribeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -35,16 +33,17 @@ public class ChatController {
 
     @ResponseBody
     @GetMapping("/chat/subscribe/{artistuuid}")
-    public String subscriptionCheck(@DestinationVariable UUID artistuuid){
+    public Subscribe subscriptionCheck(@DestinationVariable UUID artistuuid){
         UUID useruuid=UUID.randomUUID();
         artistuuid=UUID.randomUUID();
         // subscribe 테이블에서 사용자가 해당 artist를 구독중인치 체크.
         Subscribe subscription = subscribeService.getSubscription(useruuid, artistuuid);
         if (subscription!=null){
-            return subscription.getUser().getName();
+            // 구독중이라면 구독 데이터 반환
+            return subscription;
         }
-        // 만약 아니라면 다른 페이지로 리다이렉트 시킨다.
-        return "찾는 구독 정보가 없습니다!";    // 구독중이라면 해당 아티스트와의 웹소켓 통신 연결
+        // 만약 아니라면 널 값을 반환
+        return null;
     }
 
     @MessageMapping("/sendMessage/{user}")
@@ -68,7 +67,6 @@ public class ChatController {
     public Message userMessage(@DestinationVariable String artistuuid, Message message){
         // 유저가 보낸 메세지를 아티스트에게 전달하는 메서드
         // 유저가 구독한 artist의 uuid를 구독 테이블에서 확인하고
-
         return message;
     }
 }
