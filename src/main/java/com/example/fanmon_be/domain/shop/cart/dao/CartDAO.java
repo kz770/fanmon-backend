6efd046d12efbb6afc93f7d2559629021f4429b6
ccc700 <700eeu@gmail.com>
2024-10-02 17:23:59 +0900

    [Modify] 으악 DAO 변수 잘못썼어

--- src/main/java/com/example/fanmon_be/domain/shop/cart/dao/CartDAO.java
+++ src/main/java/com/example/fanmon_be/domain/shop/cart/dao/CartDAO.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CartDAO extends JpaRepository<Cart, Long> {
+public interface CartDAO extends JpaRepository<Cart, UUID> {
 
 
 //    public List<Cart> findById(UUID uuid);
