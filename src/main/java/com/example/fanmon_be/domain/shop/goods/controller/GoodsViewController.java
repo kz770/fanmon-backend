9b71a97f2c6786a25f057533815b955667f7ec57
ccc700 <700eeu@gmail.com>
2024-10-02 10:23:43 +0900

    [BugFix] 컨트롤러, 서비스 이름 수정

--- src/main/java/com/example/fanmon_be/domain/shop/goods/controller/GoodsViewController.java
+++ src/main/java/com/example/fanmon_be/domain/shop/goods/controller/GoodsViewController.java
@@ -1,30 +1,29 @@
 package com.example.fanmon_be.domain.shop.goods.controller;
 
-import com.example.fanmon_be.domain.shop.goods.service.GoodsService;
+import com.example.fanmon_be.domain.shop.goods.service.GoodsViewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
 import java.util.UUID;
 
 @Controller
-public class GoodsController {
+public class GoodsViewController {
 
     @Autowired
-    private GoodsService goodsService;
+    private GoodsViewService goodsViewService;
 
     //굿즈 목록 출력
     @GetMapping("/shop/goods")
     public void findAll(Model model) {
-        model.addAttribute("goods", goodsService.findAll());
+        model.addAttribute("goods", goodsViewService.findAll());
     }
 
     //굿즈 상세 정보 출력
     @GetMapping("/shop/goods/detail")
     public String findByID(UUID uuid, Model model) {
-        model.addAttribute("goods", goodsService.findById(uuid));
+        model.addAttribute("goods", goodsViewService.findById(uuid));
         return "/shop/goods/detail";
     }
 }
