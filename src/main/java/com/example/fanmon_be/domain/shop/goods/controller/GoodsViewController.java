1a61c4cc23486f77c257c1f9c265c7c3eaccecd8
ccc700 <700eeu@gmail.com>
2024-10-04 12:08:26 +0900

    [Modify] goodslist 나오도록 컨트롤러 수정

--- src/main/java/com/example/fanmon_be/domain/shop/goods/controller/GoodsViewController.java
+++ src/main/java/com/example/fanmon_be/domain/shop/goods/controller/GoodsViewController.java
@@ -1,14 +1,18 @@
 package com.example.fanmon_be.domain.shop.goods.controller;
 
+import com.example.fanmon_be.domain.shop.goods.entity.Goods;
 import com.example.fanmon_be.domain.shop.goods.service.GoodsViewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 public class GoodsViewController {
 
     @Autowired
@@ -16,8 +20,9 @@ public class GoodsViewController {
 
     //굿즈 목록 출력
     @GetMapping("/shop/goods")
-    public void findAll(Model model) {
-        model.addAttribute("goods", goodsViewService.findAll());
+    public ResponseEntity<List<Goods>> findAll() {
+        List<Goods> goodsList = goodsViewService.findAll(); // 서비스로부터 데이터 가져오기
+        return ResponseEntity.ok(goodsList); // JSON 형식으로 데이터 반환
     }
 
     //굿즈 상세 정보 출력
