fbe9fdd7cb4f4b2119870a11ea3d606cd828496f
nk2200 <gaeun514@gmail.com>
2024-10-07 21:54:35 +0900

    [Feat] Management Goods, Team 더보기기능
    - Goods RUD 구현
    - Team RUD 구현

--- src/main/java/com/example/fanmon_be/domain/artist/dao/ArtistDAO.java
+++ src/main/java/com/example/fanmon_be/domain/artist/dao/ArtistDAO.java
@@ -4,8 +4,13 @@ import com.example.fanmon_be.domain.artist.entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ArtistDAO extends JpaRepository<Artist, UUID> {
+
+    List<Artist> findArtistsByManagementManagementuuid(UUID managementuuid);
+
+    Artist findByArtistuuid(UUID artistuuid);
 }
--- src/main/java/com/example/fanmon_be/domain/shop/goods/dao/GoodsDAO.java
+++ src/main/java/com/example/fanmon_be/domain/shop/goods/dao/GoodsDAO.java
@@ -4,6 +4,7 @@ import com.example.fanmon_be.domain.artist.entity.Team;
 import com.example.fanmon_be.domain.shop.goods.entity.Goods;
 import com.example.fanmon_be.domain.shop.goods.enums.GoodsCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +16,8 @@ public interface GoodsDAO extends JpaRepository<Goods, UUID> {
     List<Goods> findByTeam(Team team);
 
     List<Goods> findByTeamAndCategory(Team team, GoodsCategory category);
+
+    List<Goods> findByTeamTeamuuid(UUID teamuuid);
+    @Modifying
+    void deleteGoodsByGoodsuuid(UUID goodsuuid);
 }
