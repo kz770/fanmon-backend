63c7f24323c30d971521fcdaa2c842d23b1a75d2
ccc700 <700eeu@gmail.com>
2024-10-11 15:49:59 +0900

    [Feat] 카트 삭제 밑바밥깔기

---
+++ src/main/java/com/example/fanmon_be/domain/shop/buy/controller/BuyingController.java
@@ -0,0 +1,39 @@
+package com.example.fanmon_be.domain.shop.buy.controller;
+
+import com.example.fanmon_be.domain.shop.buy.entity.Cardinfo;
+import com.example.fanmon_be.domain.shop.buy.service.CardinfoService;
+import com.example.fanmon_be.domain.shop.buy.service.OrdersService;
+import com.example.fanmon_be.domain.shop.buy.service.OrdersdetailService;
+import com.example.fanmon_be.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/shop/buy")
+public class BuyingController {
+
+    @Autowired
+    OrdersService ordersService;
+
+    @Autowired
+    OrdersdetailService ordersdetailService;
+
+    @Autowired
+    CardinfoService cardinfoService;
+
+    @Autowired
+    UserService userService;
+
+    //결제 성공
+    @PostMapping("/bought/{useruuid}")
+    public void successBuying(@PathVariable String useruuid) {
+
+    }
+
+}
+
+
