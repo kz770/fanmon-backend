a23651bd0529d6d2e67c57d4543e64dc471f4865
qordpsem <qordpsem@gmail.com>
2024-10-14 12:50:06 +0900

    [Bugfix] 원상복구

---
+++ src/main/java/com/example/fanmon_be/global/config/CorsMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.fanmon_be.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Value("${spring.web.cors.allowed-origins}")
+    private String allowedOrigins;
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(allowedOrigins.split(",")) // ,로 구분된 다수의 도메인 지원
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
